41ed7d320c3fe6ceed9a2d5ae01b1002e32da8b9
Antton Ducos <antton.ducos@epitech.eu>
2025-07-04 19:31:35 +0200

    fix(look_action): find good location in the local map to put what we saw

--- src/actions/look_action.py
+++ src/actions/look_action.py
@@ -7,6 +7,20 @@ class LookAction(Action):
     def execute(self, agent):
         print("look")
         #agent.queue_command("Look")
+    
+    def apply(self, state):
+        state["tick"] += self.cost
+        pos = state["pos"]
+        map_ = state["map"]
+        tick = state["tick"]
+        state["inventory"]["food"] = max(0, new_state["inventory"].get("food", 0) - cost)
+
+        tile = map_.get_tile(*pos)
+        if tile:
+            tile["last_seen"] = tick
+
+        return state
+
 
     def update_map_from_look_response(self, state, response_str):
         tiles_str = response_str.strip("[] \n").split(',')
@@ -17,8 +31,8 @@ class LookAction(Action):
         direction = state["dir"]
         level = state["level"]
 
-        visible_positions = get_visible_tiles_positions(pos_x, pos_y, direction, level, map_.width, map_.height)
-        
+        visible_positions = self.get_visible_tiles_positions(pos_x, pos_y, direction, level, map_.width, map_.height)
+
         for i, tile_content in enumerate(tiles_str):
             stones = {}
             players = 0
@@ -29,13 +43,15 @@ class LookAction(Action):
                     players += 1
                 else:
                     stones[obj] = stones.get(obj, 0) + 1
-            
+
             x, y = visible_positions[i]
+
             map_.update_tile(x, y, stones, players, tick)
+
         
         return state
 
-
+    @staticmethod
     def get_visible_tiles_positions(x, y, direction, level, width, height):
         positions = []
 
@@ -47,20 +63,23 @@ class LookAction(Action):
             left = (1, 0)
         elif direction == 'E':
             forward = (1, 0)
-            left = (0, -1)
+            left = (0, 1)
         elif direction == 'W':
             forward = (-1, 0)
-            left = (0, 1)
+            left = (0, -1)
         else:
             raise ValueError(f"Direction inconnue {direction}")
 
         for dist in range(level + 1):
-            center_x = (x + forward[0] * dist) % width
-            center_y = (y + forward[1] * dist) % height
+            center_x = x + forward[0] * dist
+            center_y = y + forward[1] * dist
 
             for offset in range(-dist, dist + 1):
-                tile_x = (center_x + left[0] * offset) % width
-                tile_y = (center_y + left[1] * offset) % height
+                tile_x = center_x + left[0] * offset
+                tile_y = center_y + left[1] * offset
                 positions.append((tile_x, tile_y))
 
         return positions
+    
+    def __repr__(self):
+        return f"<LookAction (cost={self.cost})>"
