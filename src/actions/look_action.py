04b50c32f9f2fb5ee171526b461e7d8f4157abc3
Antton Ducos <antton.ducos@epitech.eu>
2025-07-05 21:34:04 +0200

    fix(look update): apply perfectly the look in the local map

--- src/actions/look_action.py
+++ src/actions/look_action.py
@@ -37,7 +37,18 @@ class LookAction(Action):
         direction = state["dir"]
         level = state["level"]
 
-        visible_positions = self.get_visible_tiles_positions(pos_x, pos_y, direction, level, map_.width, map_.height)
+        visible_positions = self.get_visible_tiles_positions(
+            pos_x, pos_y, direction, level, map_.width, map_.height
+        )
+
+        if direction in ("S", "E"):
+            index = 1
+            for dist in range(1, level + 1):
+                line_len = 2 * dist + 1
+                start = index
+                end = index + line_len
+                visible_positions[start:end] = list(reversed(visible_positions[start:end]))
+                index += line_len
 
         for i, tile_content in enumerate(tiles_str):
             stones = {}
@@ -66,15 +77,19 @@ class LookAction(Action):
         if direction == 'N':
             forward = (0, -1)
             left = (-1, 0)
+            flip = False
         elif direction == 'S':
             forward = (0, 1)
             left = (1, 0)
+            flip = True
         elif direction == 'E':
             forward = (1, 0)
             left = (0, 1)
+            flip = False
         elif direction == 'W':
             forward = (-1, 0)
             left = (0, -1)
+            flip = True
         else:
             raise ValueError(f"Direction inconnue {direction}")
 
@@ -82,7 +97,11 @@ class LookAction(Action):
             center_x = x + forward[0] * dist
             center_y = y + forward[1] * dist
 
-            for offset in range(-dist, dist + 1):
+            offsets = range(-dist, dist + 1)
+            if not flip:
+                offsets = reversed(offsets)
+
+            for offset in offsets:
                 tile_x = (center_x + left[0] * offset) % width
                 tile_y = (center_y + left[1] * offset) % height
                 positions.append((tile_x, tile_y))
